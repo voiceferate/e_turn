0e76f3edfa95d0768f34a55e1ca166546d0972d0
voiceferate <vanyavi06@gmail.com>
2020-01-15 21:27:39 +0200

    deploy fixes

---
+++ config/keys.dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+  mongoURI: 'mongodb+srv://voiceferate:vxTRV5u6vGVSCckT@fullstack-ygdhd.mongodb.net/test?retryWrites=true&w=majority',
+  user: 'voiceferate',
+  password: 'vxTRV5u6vGVSCckT',
+  jwt: 'dev-jwt'
+}
--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,5 @@
-module.exports = {
-  mongoURI: 'mongodb+srv://voiceferate:vxTRV5u6vGVSCckT@fullstack-ygdhd.mongodb.net/test?retryWrites=true&w=majority',
-  user: 'voiceferate',
-  password: 'vxTRV5u6vGVSCckT',
-  jwt: 'dev-jwt'
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys.prod')
+} else {
+  module.exports = require('./keys.dev')
 }
