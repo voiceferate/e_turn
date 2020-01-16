5d565fef06bd97b73080e7b4c6c1dcf1c44ccf5a
voiceferate <vanyavi06@gmail.com>
2020-01-16 07:35:26 +0200

    fixing mongo start on heroku

--- config/keys.prod.js
+++ config/keys.prod.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: process.env.MONGO_URI,
+  mongoURI: 'mongodb+srv://voiceferate:vxTRV5u6vGVSCckT@fullstack-ygdhd.mongodb.net/test?retryWrites=true&w=majority',
   jwt: process.env.JWT
 }
