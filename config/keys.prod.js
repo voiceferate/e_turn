65e2fc0a98c81961169b21fb39bfb5edb8779f3f
voiceferate <vanyavi06@gmail.com>
2020-01-16 07:43:03 +0200

    prod keys fixing for deploy

--- config/keys.prod.js
+++ config/keys.prod.js
@@ -1,4 +1,4 @@
 module.exports = {
   mongoURI: 'mongodb+srv://voiceferate:vxTRV5u6vGVSCckT@fullstack-ygdhd.mongodb.net/test?retryWrites=true&w=majority',
-  jwt: process.env.JWT
+  jwt: 'dev-jwt'
 }
