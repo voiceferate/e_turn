9adad3c6153f3e972f1b33f75ab8f855ed87ffa7
voiceferate <vanyavi06@gmail.com>
2020-01-18 09:27:37 +0200

    added loader before app initialize spinner

--- app.js
+++ app.js
@@ -50,6 +50,17 @@ if (process.env.NODE_ENV === 'production') {
   })
 }
 
+// if (process.env.NODE_ENV === 'development') {
+//   app.use(express.static('client/dist/client'))
+
+//   app.get('*', (req, res) => {
+//     res.sendFile(
+//       path.resolve(
+//         __dirname, 'client', 'dist', 'client', 'index.html'
+//       )
+//     )
+//   })
+// }
 
 
 module.exports = app
