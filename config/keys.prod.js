83cd6d767cf961efa0a0b920539f1aa7d9be41fd
voiceferate <vanyavi06@gmail.com>
2020-01-20 21:29:29 +0200

    keys problem fix in progress

--- config/keys.prod.js
+++ config/keys.prod.js
@@ -1,6 +1,10 @@
 module.exports = {
-  mongoURI: 'mongodb+srv://voiceferate:vxTRV5u6vGVSCckT@fullstack-ygdhd.mongodb.net/test?retryWrites=true&w=majority',
-  jwt: 'dev-jwt',
-  recaptchaSecret: '6LePG9EUAAAAAHNaF29xXWoIiVUacN34RRNvVZgn',
-  recaptchaPublic: '6LePG9EUAAAAAI1etO6bfExjQdPcORcRiX98x0At'
+  mongoURI: process.env.MONGO_URI,
+  // mongoURI: 'mongodb+srv://voiceferate:vxTRV5u6vGVSCckT@fullstack-ygdhd.mongodb.net/test?retryWrites=true&w=majority',
+  jwt: process.env.JWT,
+  // jwt: 'dev-jwt',
+  recaptchaSecret: process.env.RECAPTCHA_SECRET,
+  // recaptchaSecret: '6LePG9EUAAAAAHNaF29xXWoIiVUacN34RRNvVZgn',
+  recaptchaPublic: process.env.RECAPTCHA_PUBLIC
+  // recaptchaPublic: '6LePG9EUAAAAAI1etO6bfExjQdPcORcRiX98x0At'
 }
